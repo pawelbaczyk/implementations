66d567cbd8be7ff8b8b9d6a523efad62b582488b
pawelbaczyk <baczykpawel@gmail.com>
2021-11-07 19:42:52 +0100

    automatic gradient calculation imported from numdifftools

--- hmc.py
+++ hmc.py
@@ -1,12 +1,18 @@
 import numpy
 import numpy as np
+import numdifftools
 
 
 class HMC:
-    def __init__(self, p, grad, theta_zero):
-        self.p = p
-        self.grad = grad
-        self.theta_m_minus_one = theta_zero
+    def __init__(self, probability, theta_zero):
+        self._probability = probability
+        self._theta_m_minus_one = theta_zero
+
+    def _log_p(self, args, *kwargs):
+        return numpy.log(self._probability(args, *kwargs))
+
+    def _grad_of_log_p(self, args, *kwargs):
+        return numdifftools.Gradient(self._log_p)(args, *kwargs)
 
     def sample(self, number_of_samples, epsilon, L):
         samples = []
@@ -17,29 +23,29 @@ class HMC:
         return numpy.array(samples)
 
     def _get_single_sample(self, epsilon, L):
-        theta_tilde = theta_m = self.theta_m_minus_one
+        theta_tilde = theta_m = self._theta_m_minus_one
         r_tilde = r_zero = self._sample_momentum(theta_m)
 
         for _ in range(L):
             theta_tilde, r_tilde = self._leapfrog(theta_tilde, r_tilde, epsilon)
 
         if self._is_accepted_by_metropolis(theta_tilde, r_tilde, r_zero):
-            self.theta_m_minus_one = theta_tilde
+            self._theta_m_minus_one = theta_tilde
             return theta_tilde
 
     def _is_accepted_by_metropolis(self, theta_tilde, r_tilde, r_zero):
-        alpha = np.minimum(1, self._exponent(theta_tilde, r_tilde) / self._exponent(self.theta_m_minus_one, r_zero))
+        alpha = np.minimum(1, self._exponent(theta_tilde, r_tilde) / self._exponent(self._theta_m_minus_one, r_zero))
         return numpy.random.uniform() < alpha
 
     def _exponent(self, theta, r):
-        return numpy.exp(self.p(theta) - 1 / 2 * numpy.dot(r, r))
+        return numpy.exp(self._log_p(theta) - 1 / 2 * numpy.dot(r, r))
 
     @staticmethod
     def _sample_momentum(theta):
         return numpy.random.multivariate_normal(mean=numpy.zeros_like(theta), cov=numpy.eye(N=len(theta)))
 
     def _leapfrog(self, theta, r, epsilon):
-        r_tilde = r + epsilon / 2 * self.grad(theta)
+        r_tilde = r + epsilon / 2 * self._grad_of_log_p(theta)
         theta_tilde = theta + epsilon * r_tilde
-        r_tilde += epsilon / 2 * self.grad(theta_tilde)
+        r_tilde += epsilon / 2 * self._grad_of_log_p(theta_tilde)
         return theta_tilde, r_tilde
--- tests/test_hmc.py
+++ tests/test_hmc.py
@@ -1,32 +1,24 @@
 import unittest
 from hmc import HMC
 import numpy
-import matplotlib.pyplot as plt
 
 
 class HMCTestCase(unittest.TestCase):
     def test_initialization_of_hmc(self):
         def func(theta):
-            return numpy.log(numpy.exp(-(numpy.dot(theta, theta))) / (2 * numpy.pi))
-
-        def grad(theta):
-            return 1 / func(theta) * -2 * func(theta) * theta
+            return numpy.exp(-(numpy.dot(theta, theta))) / (2 * numpy.pi)
 
         theta_zero = numpy.array([0, 0])
 
-        sampler = HMC(p=func, grad=grad, theta_zero=theta_zero)
+        sampler = HMC(probability=func, theta_zero=theta_zero)
 
     def test_sample(self):
         def func(theta):
-            return numpy.log(numpy.exp(-1 / 2 * (numpy.dot(theta, theta))) / (2 * numpy.pi))
-
-        def grad(theta):
-            return 1 / func(theta) * -2 * func(theta) * theta
+            return numpy.exp(-1 / 2 * (numpy.dot(theta, theta))) / (2 * numpy.pi)
 
         theta_zero = numpy.array([0, 0])
         N = 100
 
-        sampler = HMC(p=func, grad=grad, theta_zero=theta_zero)
+        sampler = HMC(probability=func, theta_zero=theta_zero)
         samples = sampler.sample(number_of_samples=N, epsilon=0.1, L=3)
         self.assertEqual(len(samples), N)
-
